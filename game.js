8e6b5b47cd4229ed3b53c7acf8e6dd66c776eb29
Paul Chote <paul@chote.net>
2011-03-26 16:19:59 +1300

    Clean things up a bit

--- game.js
+++ game.js
@@ -1,56 +1,50 @@
 // Tile definitions
-var wall = {
-    "allowPlace" : false,
-    "allowBeam" : false,
-    "color" : "black"
-}
+var tiles = {
+    "wall" : {
+        "allowPlace" : false,
+        "allowBeam" : false,
+        "color" : "black"
+    },
 
-var floor = {
-    "allowPlace" : true,
-    "allowBeam" : true,
-    "color" : "gray"
+    "floor" : {
+        "allowPlace" : true,
+        "allowBeam" : true,
+        "color" : "gray"
+    }
 }
 
 // Objects
-var emitter = {
-    "color" : "red"
-}
-
-var target = {
-    "color" : "green"
+var objects = {
+    "emitter" : {
+        "color" : "red"
+    },
+    "target" : {
+        "color" : "green"
+    }
 }
 
 // Cells
-var floorCell = {
-    "tile" : floor
-};
-
-var wallCell = {
-    "tile" : wall
+function Cell(tile, object) {
+    this.tile = tile;
+    this.object = object;
+    
+    this.draw = function(x,y,ctx) {
+        ctx.fillStyle = tile.color;
+        ctx.fillRect(x*32, y*32, 32, 32);
+        
+        if (object)
+        {
+            ctx.fillStyle = object.color;
+            ctx.fillRect(x*32+4, y*32+4, 24, 24);
+        }
+    }
 }
 
-var emitterCell = {
-    "tile" : floor,
-    "object" : emitter
-}
-
-var targetCell = {
-    "tile" : floor,
-    "object" : target
-}
-
-var emitter = {
-    "allowPlace" : false,
-    "allowBeam" : false,
-    "color" : "red"
-}
-
-var target = {
-    "allowPlace" : false,
-    "allowBeam" : true,
-    "color" : "green"
-}
-
+// Level definition
+var floorCell = new Cell(tiles.floor, undefined);
+var wallCell = new Cell(tiles.wall, undefined);
+var emitterCell = new Cell(tiles.floor, objects.emitter);
+var targetCell = new Cell(tiles.floor, objects.target);
 
 var map = {
     "width" : 6,
@@ -60,6 +54,10 @@ var map = {
                wallCell,emitterCell,floorCell,floorCell,floorCell,wallCell,
                wallCell,wallCell,wallCell,wallCell,wallCell,wallCell],
 }
+
+
+
+
 
 $(function() {
 	setInterval( "main()", 50 );
@@ -72,21 +70,9 @@ function main()
     ctx.clearRect(0,0, 256, 256);
 
     // Draw the map
-    renderMapLayer(ctx);
+    for (var y = 0; y < map.height; y++)
+        for (var x = 0; x < map.width; x++)
+            map.cells[y*map.width + x].draw(x,y,ctx);
         
     // Draw lasers
-}
-
-function renderMapLayer(ctx)
-{
-    for (var y = 0; y < map.height; y++)
-        for (var x = 0; x < map.width; x++){
-            var cell = map.cells[y*map.width + x];
-            ctx.fillStyle = cell.tile.color;
-            ctx.fillRect(x*32, y*32, 32, 32);
-            if (cell.object) {
-                ctx.fillStyle = cell.object.color;
-                ctx.fillRect(x*32+4, y*32+4, 24, 24);
-            }
-        }
 }
