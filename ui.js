4abd581d901f2f86673e5bb24eb94380cac56c36
Matthew Bowra-Dean <matthew@ijw.co.nz>
2011-03-26 23:46:30 +1300

    Actor replacement.

--- ui.js
+++ ui.js
@@ -1,6 +1,7 @@
 var tilePanels = new Array();
 var actorPanels = new Array();
 var tileSize = 32;
+var isTileSelected = false;
 $(function() { 
 	var ctx = $('#uiSurface')[0].getContext('2d');
 	for (var tile in tiles) {
@@ -35,6 +36,22 @@ function drawPanels() {
 	}
 }
 
+function checkBounds(x, y, panelArray, yOffset, ctx) {
+	var found = false;
+	for (var i = 0; i < panelArray.length; i++) {
+		if (x >= (i * tileSize) && x < ((i + 1) * tileSize) &&
+			y >= yOffset && y < (tileSize + yOffset)) {
+			selectedPanel = panelArray[i];
+			found = true;
+			ctx.lineWidth = 3;
+			ctx.strokeStyle = "limegreen";
+			ctx.strokeRect(i * tileSize + 1, yOffset + 1,
+				tileSize - 2, tileSize - 2);
+		}
+	}
+	return found;
+}
+
 function uiClick(e) {
 	var ui = $('#uiSurface');
 	var x = e.pageX - ui.offset().left;
@@ -44,26 +61,8 @@ function uiClick(e) {
 	ctx.clearRect(0,0,256,100);
 	drawPanels();
 	selectedPanel = null;
-	for (var i = 0; i < tilePanels.length; i++) {
-		if (x >= (i * tileSize) && x < ((i + 1) * tileSize) && 
-			y >= 0 && y < tileSize) {
-			selectedPanel = tilePanels[i];
-			ctx.lineWidth = 3;
-			ctx.strokeStyle = "limegreen";
-			ctx.strokeRect(i * tileSize + 1, 0 + 1, 
-				tileSize - 2, tileSize - 2);
-		}
-	}
-	for (var i = 0; i < actorPanels.length; i++) {
-		if (x >= (i * tileSize) && x < ((i + 1) * tileSize) &&
-			y >= tileSize && y < (tileSize * 2)) {
-			selectedPanel = actorPanels[i];
-			ctx.lineWidth = 3;
-			ctx.strokeStyle = "limegreen";
-			ctx.strokeRect(i * tileSize + 1, tileSize + 1,
-				tileSize - 2, tileSize - 2);
-		}
-	}
+	if (checkBounds(x, y, tilePanels, 0, ctx)) isTileSelected = true;
+	if (checkBounds(x, y, actorPanels, tileSize, ctx)) isTileSelected = false;
 }
 
 function gameSurfaceMove(e) {
@@ -80,5 +79,8 @@ function gameSurfaceClick(e) {
 	if (selectedPanel == null || nearestTileX == null) return;
 	var xIndex = nearestTileX / tileSize;
 	var yIndex = nearestTileY / tileSize;
-	map.getCell([xIndex,yIndex]).tile = selectedPanel;
+	if (isTileSelected)
+		map.getCell([xIndex,yIndex]).tile = selectedPanel;
+	else
+		map.getCell([xIndex,yIndex]).actor = selectedPanel;
 }
