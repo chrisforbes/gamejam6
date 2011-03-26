a3b3b7bfb3476b744e466d1549c586e6c3cb67af
Paul Chote <paul@chote.net>
2011-03-26 18:55:34 +1300

    Implement all 1-side mirrors and laser emitters

--- game.js
+++ game.js
@@ -36,10 +36,34 @@ function Target(origin) {
     this.origin = origin;
 }
 
-function Laser(origin,direction) {
-    this.origin = origin;
-    this.direction = direction;
-    
+function Laser(color,dir) {
+    this.color = color;
+    var c = 4;
+    switch(color) {
+        case "red": c = 4; break;
+        case "green": c = 2; break;
+        case "blue": c = 3; break;
+        case "white": c = 1; break;
+    }
+    switch (dir) {
+        case "n":
+            this.origin = [7,c];
+            this.direction = [0,-1];
+        break;
+        case "e":
+            this.origin = [4,c];
+            this.direction = [1,0];
+        break;
+        case "s":
+            this.origin = [5,c];
+            this.direction = [0,1];
+        break;
+        case "w":
+            this.origin = [6,c];
+            this.direction = [-1,0];
+        break;
+    }
+
     this.shoot = function(x,y,ctx) {
         ctx.fillStyle = "rgba(128,128,255,0.75)";
         var curDir = [this.direction[0], this.direction[1]];
@@ -52,7 +76,7 @@ function Laser(origin,direction) {
                 ctx.fillRect(cx*32+11, cy*32+11, 10,10)
                 break;
             }
-            drawBeam([cx,cy], curDir, newDir, ctx);
+            drawBeam(this.color, [cx,cy], curDir, newDir, ctx);
             curDir = newDir;
             cx += curDir[0];
             cy += curDir[1];
@@ -65,16 +89,25 @@ function isEqualDir(d1, d2) {
     return d1[0] == d2[0] && d1[1] == d2[1];
 }
 
-// origin = artwork origin
-// dirs = array of dir -> dir for in/out
-function Mirror(origin,dirs) {
-    this.origin = origin;
-    this.dirs = dirs;
+function Mirror(type) {
+    if (type == "nw") {
+        this.origin = [2,0];
+        this.dirs = [[[1,0],[0,-1]],[[0,1],[-1,0]]];
+    } else if (type == "sw") {
+        this.origin = [2,1];
+        this.dirs = [[[0,-1],[-1,0]],[[1,0],[0,1]]];
+    } else if (type == "ne") {
+        this.origin = [3,0];
+        this.dirs = [[[0,1],[1,0]],[[-1,0],[0,-1]]];
+    } else if (type == "se") {
+        this.origin = [3,1];
+        this.dirs = [[[0,-1],[1,0]],[[-1,0],[0,1]]];
+    }
     
     this.beamExitDirection = function(dir) {
-        for (var d in dirs) {
-            if (isEqualDir(dir, dirs[d][0]))
-                return dirs[d][1];
+        for (var d in this.dirs) {
+            if (isEqualDir(dir, this.dirs[d][0]))
+                return this.dirs[d][1];
         }
             
         return undefined;
@@ -104,15 +137,19 @@ function Cell(tile, object) {
     }
 }
 
-function drawBeam(xy, d0, d1, ctx) {
+function drawBeam(color, xy, d0, d1, ctx) {
     var origin = undefined;
     for (var i in beamArt)
         if (isEqualDir(beamArt[i][0],d0) && isEqualDir(beamArt[i][1],d1)) {
             origin = beamArt[i][2];
             break;
         }
-    
-    ctx.drawImage(imageSheet, 32*origin[0], 32*origin[1], 32, 32, xy[0]*32, xy[1]*32, 32, 32);
+    var o = 0;
+    switch(color) {
+        case "green": o = 128; break;
+        case "blue": o = 64; break;
+    }
+    ctx.drawImage(imageSheet, 32*origin[0], 32*origin[1]+o, 32, 32, xy[0]*32, xy[1]*32, 32, 32);
 }
 
 
@@ -120,17 +157,19 @@ function drawBeam(xy, d0, d1, ctx) {
 // Level definition
 var floorCell = new Cell(tiles.floor, undefined);
 var wallCell = new Cell(tiles.wall, undefined);
-var emitterCell = new Cell(tiles.floor, new Laser([4,4], [1,0]));
+var emitterCell = new Cell(tiles.floor, new Laser("green", "e"));
 var targetCell = new Cell(tiles.floor, new Target([4,6]));
-var mirrorCell = new Cell(tiles.floor, new Mirror([2,0], [[[1,0],[0,-1]],[[0,1],[-1,0]]]));
-var mirror2Cell = new Cell(tiles.floor, new Mirror([2,1], [[[0,-1],[-1,0]],[[1,0],[0,1]]]));
+var mirrorNWCell = new Cell(tiles.floor, new Mirror("nw"));
+var mirrorNECell = new Cell(tiles.floor, new Mirror("ne"));
+var mirrorSECell = new Cell(tiles.floor, new Mirror("se"));
+var mirrorSWCell = new Cell(tiles.floor, new Mirror("sw"));
 
 var map = {
     "width" : 6,
     "height" : 4,
     "cells" : [wallCell,wallCell,wallCell,wallCell,wallCell,wallCell,
-               wallCell,targetCell,floorCell,mirror2Cell,floorCell,wallCell,
-               wallCell,emitterCell,floorCell,mirrorCell,floorCell,wallCell,
+               wallCell,targetCell,floorCell,mirrorSECell,mirrorSWCell,wallCell,
+               wallCell,floorCell,emitterCell,mirrorNWCell,mirrorNECell,wallCell,
                wallCell,wallCell,wallCell,wallCell,wallCell,wallCell],
     "getCell" : function(xy) { return this.cells[xy[1]*map.width + xy[0]]; }
 }
@@ -153,5 +192,5 @@ function main()
             
     
     // Draw lasers
-    map.getCell([1,2]).object.shoot(1,2,ctx);
+    map.getCell([2,2]).object.shoot(2,2,ctx);
 }
